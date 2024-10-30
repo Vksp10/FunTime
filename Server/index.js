2e052ea7d40f2e342e68962b1ae23bcfc1bb6ea9
Vaibhav <vaibhavkashyap1012@gmail.com>
2024-10-30 22:44:43 +0530

    Final Commit

--- Server/index.js
+++ Server/index.js
@@ -8,6 +8,8 @@ import UserRoute from './Routes/UserRoute.js';
 import PostRoute from './Routes/PostRoute.js';
 import UploadRoute from './Routes/UploadRoute.js';
 
+// Configuring environment variables
+dotenv.config();
 
 // Routes
 const app = express();
@@ -23,15 +25,15 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-dotenv.config();
+mongoose.connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+}).catch((error) => {
+    console.error("MongoDB connection error:", error.message); // This will show the actual error message
+});
 
-mongoose.connect
-    (process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true }
-    ).then(() =>
-        app.listen(process.env.PORT, () => console.log(`listening at ${process.env.PORT}`))
-    ).catch((error) =>
-        console.log('error')
-    )
 
 
 // uses of routes
@@ -40,3 +42,4 @@ app.use('/auth', AuthRoute);
 app.use('/user', UserRoute);
 app.use('/post', PostRoute);
 app.use('/upload', UploadRoute);
+
